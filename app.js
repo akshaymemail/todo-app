b1209322f79823ca516a285ad0773392b43bbee4
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2021-01-17 12:05:46 +0530

    app data is now hosted on mongodb atlas (aws)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const _ = require("lodash")
 const date = require(__dirname + "/date.js")
 const port = 3000
 
@@ -12,55 +13,92 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use(express.static("public"))
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+mongoose.connect("mongodb+srv://admin-akshay:mongodb6@akshay.vlzrm.mongodb.net/todolistDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
-const todoListSchema = new mongoose.Schema({
+const homeListSchema = {
     name: String
-})
+}
 
-const Items = mongoose.model("Item", todoListSchema)
+anonoymousSchema = {
+    listName: {
+        type: String
+    },
+    listItems: {
+        type: [homeListSchema]
+    }
+
+}
+
+const Items = mongoose.model("Item", homeListSchema)
+
+const anonoymousList = mongoose.model("anonoymousList", anonoymousSchema)
 
 const buyFood = new Items({
     name: "Welcome to my todo web app"
 })
+
 const eatFood = new Items({
     name: "Use + to add new items"
 })
+
 const payMoney = new Items({
     name: "Check the checkbox to delete item"
 })
 
-let workItems = []
-
+const home = date.getExternalDate()
 app.get("/", function (req, res) {
-    let time = date.getExternalDate()
-
     Items.find({}, function (err, items) {
         if (err) {
             console.log(err)
         } else {
             if (items.length === 0) {
-                Items.insertMany([buyFood, eatFood, payMoney], function (err){
-                    if (err){
-                        console.log(err)
-                    }else{
-                        console.log("Item added successfully to the database")
-                        //res.redirect("/")
-                    }
-                })
+                Items.insertMany([
+                        buyFood,
+                        eatFood,
+                        payMoney
+                    ],
+                    function (err) {
+                        if (err) {
+                            console.log(err)
+                        }
+                    })
             }
 
             res.render("home", {
-                listTitle: time,
+                listTitle: home,
                 newListItem: items
             })
         }
     })
+})
 
-    
+app.get("/:anonoymousRoute", function (req, res) {
+    const anonoymousRoute = _.capitalize(req.params.anonoymousRoute)
+    anonoymousList.findOne({
+        listName: anonoymousRoute
+    }, function (err, result) {
+        if (!err) {
+            if (!result) {
+                // add the items in the list
+                new anonoymousList({
+                    listName: anonoymousRoute,
+                    listItems: [buyFood, eatFood, payMoney]
+                }).save()
+                res.redirect("/" + anonoymousRoute)
+            } else {
+                // rednder the items from the list
+                res.render("home", {
+                    listTitle: result.listName,
+                    newListItem: result.listItems
+                })
+            }
+
+        }
+    })
+
 })
 
 app.get("/about", function (req, res) {
@@ -68,33 +106,59 @@ app.get("/about", function (req, res) {
 })
 
 app.post("/", function (req, res) {
-    if (req.body.list.replace(/\s+/g, '') === "WorkList") {
-        workItems.push(req.body.newItem)
-        res.redirect('/work')
-    } else {
+    if (req.body.list === home) {
+        // insert new item in homepage collection db
         new Items({
             name: req.body.newItem
         }).save()
         res.redirect('/')
+    } else {
+        // insert new item in anonoymous collection db
+        anonoymousList.findOne({
+            listName: req.body.list
+        }, function (err, result) {
+            if (!err) {
+                result.listItems.push({
+                    name: req.body.newItem
+                })
+                result.save() 
+            }
+            res.redirect("/" + req.body.list)
+        })
+
     }
 
 })
 
-app.get("/work", function (req, res) {
-    res.render("home", {
-        listTitle: "Work List",
-        newListItem: workItems
-    })
-})
-
 app.post("/delete", function (req, res) {
-    Items.findByIdAndRemove(req.body.itemId,function(err){
-        if(!err) {
-            console.log("deleted")
-        }
-    })
-    res.redirect("/")
-    //  console.log(req.body.itemId)
+
+    if (req.body.listName === home) {
+        // delete items from home collection database
+        Items.findByIdAndRemove(
+            req.body.itemId,
+            function (err) {
+                if (err) {
+                    console.log(err)
+                }
+            })
+        res.redirect("/")
+    } else {
+        // delete items from anonoymousList collection database
+        anonoymousList.findOneAndUpdate({
+            listName: req.body.listName
+        }, {
+            $pull: {
+                listItems: {
+                    _id: req.body.itemId
+                }
+            }
+        }, function (err, result) {
+            if (!err) {
+                res.redirect("/" + req.body.listName)
+            }
+        })
+
+    }
 })
 
 app.listen(process.env.PORT || port, function () {
