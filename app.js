14e5c8cd5bb8672a8662a91afb86f673328d3591
Akshay Singh <43178939+akshaymemail@users.noreply.github.com>
2021-01-22 11:50:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use(express.static("public"))
-mongoose.connect("mongodb+srv://admin-akshay:mongodb6@akshay.vlzrm.mongodb.net/todolistDB", {
+mongoose.connect("your application database connection", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
