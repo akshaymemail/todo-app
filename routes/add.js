b25369364d019cb932d9232e9e2b6541807dade2
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2021-02-21 15:13:14 +0530

    complete add new data to default list of the user

--- routes/add.js
+++ routes/add.js
@@ -1,11 +1,30 @@
 const express = require('express')
 
 const ITEMS = require('../lib/items')
+const User = require('../config/db')
+const login = require('./login')
 const router = express.Router()
 
 router.post('/add', (req, res) => {
-    ITEMS.push(req.body.newItem)
-    res.redirect('/')
+    if (req.isAuthenticated()) {
+        User.findOne({_id : login.user._id}, (err, user) => {
+            if(!err) {
+                user.list.listItems.push({
+                    item : req.body.newItem
+                })
+                user.save(err => {
+                    if(!err) {
+                        res.redirect('/')
+                    }
+                })
+            }else{
+                console.log(err)
+            }
+        })
+    } else {
+        ITEMS.push(req.body.newItem)
+        res.redirect('/')
+    }
 })
 
 module.exports = router
